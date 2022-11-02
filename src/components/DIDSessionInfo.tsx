94b392b4fed809855a76954448cabb183294a448
Artur Wdowiarski <arturwdowiarski@me.com>
2022-11-02 10:53:50 +0000

    Copy update

--- src/components/DIDSessionInfo.tsx
+++ src/components/DIDSessionInfo.tsx
@@ -4,7 +4,7 @@ function DIDSessionDemoHandler() {
      <ul className='App-ul'>
        <li className='App-li'><a className='App-link' href='https://github.com/ceramicnetwork/js-did/tree/main/packages/did-session'>https://github.com/ceramicnetwork/js-did/tree/main/packages/did-session</a></li>
        <li className='App-li'>create a did:key</li>
-       <li className='App-li'>authenticate your DApp’s user with SIWE (+ support for other auth providers)</li>
+       <li className='App-li'>authenticate your DApp’s user with SIWE or SIWS (+ support for other auth providers)</li>
        <li className='App-li'>create a CACAO for your did:key, issued by the user’s did:pkh</li>
        <li className='App-li'>store it in a serializable session object</li>
      </ul>
